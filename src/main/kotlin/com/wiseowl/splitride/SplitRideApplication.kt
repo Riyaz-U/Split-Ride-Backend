b993f23b27e581fc78777daf318739216ac66e07
Riyaz Uddin <riyazps007@gmail.com>
2025-12-10 21:45:11 +0530

    initial commit🎉

---
+++ src/main/kotlin/com/wiseowl/splitride/SplitRideApplication.kt
@@ -0,0 +1,11 @@
+package com.wiseowl.splitride
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SplitRideApplication
+
+fun main(args: Array<String>) {
+    runApplication<SplitRideApplication>(*args)
+}
