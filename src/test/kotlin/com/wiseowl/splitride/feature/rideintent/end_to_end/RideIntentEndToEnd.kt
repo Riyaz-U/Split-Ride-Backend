613dd556b888bb0f12dcad014cd8367d92d2e6f1
Riyaz Uddin <riyazps007@gmail.com>
2026-02-03 23:06:29 +0530

    test: added more core test cases

--- src/test/kotlin/com/wiseowl/splitride/feature/rideintent/end_to_end/RideIntentEndToEnd.kt
+++ src/test/kotlin/com/wiseowl/splitride/feature/rideintent/end_to_end/RideIntentEndToEnd.kt
@@ -6,14 +6,15 @@ import com.wiseowl.splitride.feature.rideintent.dto.JoinGroupResponseDTO
 import com.wiseowl.splitride.feature.rideintent.dto.RideGroupDTO
 import com.wiseowl.splitride.feature.rideintent.dto.RideIntentResponseDTO
 import com.wiseowl.splitride.feature.rideintent.model.Direction
+import com.wiseowl.splitride.feature.rideintent.model.RideGroupStatus
 import com.wiseowl.splitride.feature.rideintent.model.RideIntentStatus
-import com.wiseowl.splitride.feature.rideintent.repository.RideGroupMemberRepository
-import org.junit.jupiter.api.AfterEach
+import jakarta.transaction.Transactional
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.Rollback
 import org.springframework.test.web.servlet.client.RestTestClient
 import org.springframework.test.web.servlet.client.expectBody
 import java.time.Instant
@@ -21,12 +22,22 @@ import java.time.Instant.parse
 
 @SpringBootTest
 @AutoConfigureRestTestClient
-class RideIntentEndToEnd(@Autowired val restTestClient: RestTestClient, @Autowired val repository: RideGroupMemberRepository) {
+@Transactional
+@Rollback
+class RideIntentEndToEnd(@Autowired val restTestClient: RestTestClient) {
 
-    @AfterEach
-    fun tearDown() {
-        repository.deleteAll()
-    }
+    private val createRideIntentRequestBody = CreateRideIntentRequestDTO(
+        userId = "48b7ee6d-8f7c-4056-89c3-85557237bc21",
+        direction = Direction.HOME_TO_OFFICE,
+        sourceLat = 28.124413,
+        sourceLng = 78.124413,
+        destinationLat = 28.12441,
+        destinationLng = 78.124413,
+        sourceArea = "DLFCyberHub",
+        destinationArea = "TechPark",
+        startTime = "2026-02-28T22:14:45.000Z",
+        flexibleMinutes = 10
+    )
 
     @Test
     fun createRideIntent() {
@@ -39,7 +50,7 @@ class RideIntentEndToEnd(@Autowired val restTestClient: RestTestClient, @Autowir
             destinationLng = 78.124413,
             sourceArea = "DLFCyberHub",
             destinationArea = "TechPark",
-            startTime = Instant.now().plusSeconds(60*15).toString(),
+            startTime = Instant.now().plusSeconds(60 * 15).toString(),
             flexibleMinutes = 10
         )
 
@@ -59,43 +70,23 @@ class RideIntentEndToEnd(@Autowired val restTestClient: RestTestClient, @Autowir
 
     @Test
     fun createRideIntentWithInvalidSartTimeReturnBadRequest() {
-        val request = CreateRideIntentRequestDTO(
-            userId = "48b7ee6d-8f7c-4056-89c3-85557237bc21",
-            direction = Direction.HOME_TO_OFFICE,
-            sourceLat = 28.124413,
-            sourceLng = 78.124413,
-            destinationLat = 28.12441,
-            destinationLng = 78.124413,
-            sourceArea = "DLFCyberHub",
-            destinationArea = "TechPark",
-            startTime = Instant.now().minusSeconds(60).toString(),
-            flexibleMinutes = 10
-        )
-
         restTestClient.post().uri("/api/ride-intents")
             .contentType(MediaType.APPLICATION_JSON)
-            .body(request)
+            .body(
+                createRideIntentRequestBody
+                    .copy(
+                        startTime = Instant.now().minusSeconds(60 * 15).toString(),
+                    )
+            )
             .exchange()
             .expectStatus().isBadRequest
     }
 
     @Test
     fun joinRideIntentGroup() {
-        val request = CreateRideIntentRequestDTO(
-            userId = "48b7ee6d-8f7c-4056-89c3-85557237bc21",
-            direction = Direction.HOME_TO_OFFICE,
-            sourceLat = 28.124413,
-            sourceLng = 78.124413,
-            destinationLat = 28.12441,
-            destinationLng = 78.124413,
-            sourceArea = "DLFCyberHub",
-            destinationArea = "TechPark",
-            startTime = "2026-02-28T22:14:45.000Z",
-            flexibleMinutes = 10
-        )
         val createdRideIntent = restTestClient.post().uri("/api/ride-intents")
             .contentType(MediaType.APPLICATION_JSON)
-            .body(request)
+            .body(createRideIntentRequestBody)
             .exchange()
             .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
             .returnResult().responseBody?.data
@@ -104,41 +95,29 @@ class RideIntentEndToEnd(@Autowired val restTestClient: RestTestClient, @Autowir
             .exchange()
             .expectBody<JoinGroupResponseDTO>()
             .value { response ->
-                assert(response?.rideGroupId!=null)
-                assert(response?.isGroupFull==false)
+                assert(response?.rideGroupId != null)
+                assert(response?.isGroupFull == false)
                 assert(response?.currentMembers?.size == 1)
             }
     }
 
     @Test
     fun cancelRideIntent() {
-        val request = CreateRideIntentRequestDTO(
-            userId = "48b7ee6d-8f7c-4056-89c3-85557237bc21",
-            direction = Direction.HOME_TO_OFFICE,
-            sourceLat = 28.124413,
-            sourceLng = 78.124413,
-            destinationLat = 28.12441,
-            destinationLng = 78.124413,
-            sourceArea = "DLFCyberHub",
-            destinationArea = "TechPark",
-            startTime = "2026-02-28T22:14:45.000Z",
-            flexibleMinutes = 10
-        )
-
         val createdRideIntent = restTestClient.post().uri("/api/ride-intents")
             .contentType(MediaType.APPLICATION_JSON)
-            .body(request)
+            .body(createRideIntentRequestBody)
             .exchange()
             .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
             .value { response ->
-                assert(response?.data?.userId == request.userId)
-                assert(response?.data?.direction == request.direction)
+                assert(response?.data?.userId == createRideIntentRequestBody.userId)
+                assert(response?.data?.direction == createRideIntentRequestBody.direction)
                 assert(response?.data?.status == RideIntentStatus.ACTIVE)
-                assert(response?.data?.flexibleMinutes == request.flexibleMinutes)
-                assert(parse(response?.data?.startTime) == parse(request.startTime))
+                assert(response?.data?.flexibleMinutes == createRideIntentRequestBody.flexibleMinutes)
+                assert(parse(response?.data?.startTime) == parse(createRideIntentRequestBody.startTime))
             }.returnResult().responseBody?.data
 
-        restTestClient.post().uri("/api/ride-intents/${createdRideIntent?.id}/cancel?userId=${createdRideIntent?.userId}")
+        restTestClient.post()
+            .uri("/api/ride-intents/${createdRideIntent?.id}/cancel?userId=${createdRideIntent?.userId}")
             .exchange()
             .expectStatus()
             .isOk
@@ -146,29 +125,17 @@ class RideIntentEndToEnd(@Autowired val restTestClient: RestTestClient, @Autowir
 
     @Test
     fun cancellingRideIntentThatIsAlreadyGroupedRemovesAssociatedGroupMemberFromThGroup() {
-        val request = CreateRideIntentRequestDTO(
-            userId = "48b7ee6d-8f7c-4056-89c3-85557237bc21",
-            direction = Direction.HOME_TO_OFFICE,
-            sourceLat = 28.124413,
-            sourceLng = 78.124413,
-            destinationLat = 28.12441,
-            destinationLng = 78.124413,
-            sourceArea = "DLFCyberHub",
-            destinationArea = "TechPark",
-            startTime = "2026-02-28T22:14:45.000Z",
-            flexibleMinutes = 10
-        )
 
         val createdRideIntent1 = restTestClient.post().uri("/api/ride-intents")
             .contentType(MediaType.APPLICATION_JSON)
-            .body(request)
+            .body(createRideIntentRequestBody)
             .exchange()
             .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
             .returnResult().responseBody?.data
 
         val createdRideIntent2 = restTestClient.post().uri("/api/ride-intents")
             .contentType(MediaType.APPLICATION_JSON)
-            .body(request)
+            .body(createRideIntentRequestBody)
             .exchange()
             .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
             .returnResult().responseBody?.data
@@ -178,8 +145,8 @@ class RideIntentEndToEnd(@Autowired val restTestClient: RestTestClient, @Autowir
             .expectBody<JoinGroupResponseDTO>()
             .value { response ->
                 print(response)
-                assert(response?.rideGroupId!=null)
-                assert(response?.isGroupFull==false)
+                assert(response?.rideGroupId != null)
+                assert(response?.isGroupFull == false)
                 assert(response?.currentMembers?.size == 1)
             }
 
@@ -188,16 +155,178 @@ class RideIntentEndToEnd(@Autowired val restTestClient: RestTestClient, @Autowir
             .expectBody<JoinGroupResponseDTO>()
             .returnResult().responseBody
 
-        restTestClient.get().uri("http://localhost:8080/api/ride-intents/${createdRideIntent1?.id}/cancel?userId=${request.userId}")
+        restTestClient.get()
+            .uri("/api/ride-intents/${createdRideIntent1?.id}/cancel?userId=${createRideIntentRequestBody.userId}")
 
         restTestClient.get().uri("/api/ride-groups/info/${groupedResponse?.rideGroupId}")
             .exchange()
             .expectBody<SplitRideResponse<RideGroupDTO>>()
             .value { response ->
-                assert(response?.data?.members?.any { it.rideIntentId.toString()!=createdRideIntent1?.id }==true)
-                assert(response?.data?.members?.any { it.rideIntentId.toString()==createdRideIntent2?.id }==true)
+                assert(response?.data?.members?.any { it.rideIntentId.toString() != createdRideIntent1?.id } == true)
+                assert(response?.data?.members?.any { it.rideIntentId.toString() == createdRideIntent2?.id } == true)
             }
     }
 
+    @Test
+    fun groupFullOneIntentCancelsGroupBecomesOpen() {
+        val createdRideIntent1 = restTestClient.post().uri("/api/ride-intents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(createRideIntentRequestBody)
+            .exchange()
+            .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
+            .returnResult().responseBody?.data
 
+        val createdRideIntent2 = restTestClient.post().uri("/api/ride-intents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(createRideIntentRequestBody)
+            .exchange()
+            .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
+            .returnResult().responseBody?.data
+
+        val createdRideIntent3 = restTestClient.post().uri("/api/ride-intents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(createRideIntentRequestBody)
+            .exchange()
+            .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
+            .returnResult().responseBody?.data
+
+        restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent1?.id}")
+            .exchange()
+
+        restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent2?.id}")
+            .exchange()
+
+        val grouped3rResponse =
+            restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent3?.id}")
+                .exchange()
+                .expectBody<JoinGroupResponseDTO>()
+                .returnResult().responseBody
+
+        restTestClient.get().uri("/api/ride-groups/info/${grouped3rResponse?.rideGroupId}")
+            .exchange()
+            .expectBody<SplitRideResponse<RideGroupDTO>>()
+            .value {
+                assert(it?.data?.status == RideGroupStatus.FULL)
+            }
+
+        restTestClient.post()
+            .uri("/api/ride-intents/${createdRideIntent2?.id}/cancel?userId=${createdRideIntent2?.userId}")
+            .exchange()
+            .expectStatus()
+            .isOk
+
+        restTestClient.get().uri("/api/ride-groups/info/${grouped3rResponse?.rideGroupId}")
+            .exchange()
+            .expectBody<SplitRideResponse<RideGroupDTO>>()
+            .value {
+                assert(it?.data?.status == RideGroupStatus.OPEN)
+            }
+    }
+
+    @Test
+    fun groupHasSpaceForOne_OneRideIntentJoins_GroupBecomesFull() {
+        val createdRideIntent1 = restTestClient.post().uri("/api/ride-intents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(createRideIntentRequestBody)
+            .exchange()
+            .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
+            .returnResult().responseBody?.data
+
+        val createdRideIntent2 = restTestClient.post().uri("/api/ride-intents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(createRideIntentRequestBody)
+            .exchange()
+            .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
+            .returnResult().responseBody?.data
+
+        val createdRideIntent3 = restTestClient.post().uri("/api/ride-intents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(createRideIntentRequestBody)
+            .exchange()
+            .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
+            .returnResult().responseBody?.data
+
+        restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent1?.id}")
+            .exchange()
+
+        restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent2?.id}")
+            .exchange()
+
+        val rideGroupId = restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent3?.id}")
+            .exchange()
+            .expectBody<JoinGroupResponseDTO>()
+            .returnResult().responseBody?.rideGroupId
+
+        restTestClient.get().uri("/api/ride-groups/info/$rideGroupId")
+            .exchange()
+            .expectBody<SplitRideResponse<RideGroupDTO>>()
+            .value {
+                print(it)
+                assert(it?.data?.status == RideGroupStatus.FULL)
+            }
+    }
+
+    @Test
+    fun groupCancelWhenLastMemberLeaves() {
+        val createdRideIntent1 = restTestClient.post().uri("/api/ride-intents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(createRideIntentRequestBody)
+            .exchange()
+            .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
+            .returnResult().responseBody?.data
+
+        val createdRideIntent2 = restTestClient.post().uri("/api/ride-intents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(createRideIntentRequestBody)
+            .exchange()
+            .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
+            .returnResult().responseBody?.data
+
+        val rideGroupId = restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent1?.id}")
+            .exchange()
+            .expectBody<JoinGroupResponseDTO>()
+            .returnResult()
+            .responseBody?.rideGroupId
+
+
+        restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent2?.id}")
+            .exchange()
+
+        restTestClient.post()
+            .uri("/api/ride-intents/${createdRideIntent1?.id}/cancel?userId=${createdRideIntent1?.userId}")
+            .exchange()
+
+        restTestClient.post()
+            .uri("/api/ride-intents/${createdRideIntent2?.id}/cancel?userId=${createdRideIntent2?.userId}")
+            .exchange()
+
+        restTestClient.get().uri("/api/ride-groups/info/$rideGroupId")
+            .exchange()
+            .expectBody<SplitRideResponse<RideGroupDTO>>()
+            .value {
+                assert(it?.data?.status == RideGroupStatus.CANCELLED)
+            }
+    }
+
+    @Test
+    fun groupIntentRejectsRejoin() {
+        val rideIntentBody = createRideIntentRequestBody.copy(
+            startTime = Instant.now().plusSeconds(10).toString()
+        )
+        val createdRideIntent1 = restTestClient.post().uri("/api/ride-intents")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(rideIntentBody)
+            .exchange()
+            .expectBody<SplitRideResponse<RideIntentResponseDTO>>()
+            .returnResult().responseBody?.data
+
+
+        restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent1?.id}")
+            .exchange()
+
+        restTestClient.post().uri("/api/ride-groups/join?rideIntentId=${createdRideIntent1?.id}")
+            .exchange()
+            .expectStatus()
+            .isBadRequest
+    }
 }
